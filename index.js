dfe1182cfbbea2f072b47841f8f00c0c006427c6
narutitoo <psnarutitoo@gmail.com>
2025-07-17 23:12:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
+// index.js
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 
-// Middleware
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-// Ruta principal
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Ruta obligatoria para FreeCodeCamp
-app.get('/api/whoami', function (req, res) {
+// Endpoint principal
+app.get('/api/whoami', (req, res) => {
+  const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
   res.json({
-    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
-    language: req.headers['accept-language'],
-    software: req.headers['user-agent']
+    ipaddress: ip,
+    language: language,
+    software: software
   });
 });
 
-// Puerto
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
