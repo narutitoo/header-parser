3160c75a28aaa396753ea9242e22d4f4792bfe53
narutitoo <psnarutitoo@gmail.com>
2025-07-17 22:32:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ app.get('/', (req, res) => {
 });
 
 // Ruta requerida por el proyecto
-app.get('/api/whoami', (req, res) => {
-  const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
+app.get("/api/whoami", (req, res) => {
+  const ip = req.headers["x-forwarded-for"]?.split(',')[0] || req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
 
   res.json({
-    ipaddress,
-    language,
-    software
+    ipaddress: ip,
+    language: language,
+    software: software
   });
 });
 
